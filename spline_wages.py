3b88e697db96a630d48f2c7d7439a350371632d7
Paul Victor <paul.victor@apachecorp.com>
2020-01-24 11:12:18 -0600

    Initial Load of Data and script.

---
+++ spline_wages.py
@@ -0,0 +1,15 @@
+#%% Import Modules
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+
+#%% Read in Data
+df = pd.read_csv('Wage.csv')
+
+# %%
+df.head()
+
+# %%
