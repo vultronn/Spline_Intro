a3ddbbcf49d58ff1eacd9435664a356507337650
Paul Victor <paul.victor@apachecorp.com>
2020-01-29 16:06:01 -0600

    Added and initial jupyter notebook file

--- spline_wages.py
+++ spline_wages.py
@@ -5,6 +5,7 @@ import statsmodels.api as sm
 import matplotlib.pyplot as plt
 %matplotlib inline
 
+This notebook walks through the exercise used in https://www.analyticsvidhya.com/blog/2018/03/introduction-regression-splines-python-codes/
 
 #%% Read in Data
 df = pd.read_csv('Wage.csv')
